104fa6beb9f255400d1260fb63e0fb366e8e02cd
JBeluche <j.beluche@outlook.com>
2022-11-18 11:25:10 +0100

    Updated welcom view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -157,8 +157,10 @@ body {
 <div class="stars3" aria-hidden="true"></div>
 <main class="main">
 	<section class="contact">
-		<h1 class="title">Awesome Thing</h1>
+		<h1 class="title">Hello there</h1>
 		<h2 class="sub-title">Site Under Construction</h2>
+        <br>
+        <p>Soon we will have something great!</p>
 	</section>
 </main>
 
