b6a70633f181bacb7795e4b0c251fbdb234ba606
JBeluche <j.beluche@outlook.com>
2022-11-18 12:09:35 +0100

    Testing cors configurations again...

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080', '*'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'], //not safe?
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'], //not safe?
 
     'max_age' => 0,
 
